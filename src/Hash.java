2fa169194f0772f119812d775310b2d63d62c775
rm809 <83632267+distriful5061@users.noreply.github.com>
2022-06-18 11:57:02 +0900

    Update Hash.java

--- src/Hash.java
+++ src/Hash.java
@@ -4,9 +4,22 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Scanner;
 
+/**
+ * 入力された平文からハッシュ値を生成するクラス
+ * @author distriful5061
+ * @version 1.0.0
+ */
 public class Main {
+    /**
+     * 使用するハッシュ値のリスト
+     */
     private static final String[] HashTypes = {"MD2","MD5","SHA-1","SHA-224","SHA-256","SHA-384","SHA-512","SHA3-224","SHA3-256","SHA3-384","SHA3-512"};
 
+    /**
+     * メインのメソッド
+     * @param args 実行時に渡された引数のリスト
+     * @throws NoSuchAlgorithmException 指定されたハッシュ関数名が存在しません
+     */
     public static void main(String[] args) throws NoSuchAlgorithmException {
         Scanner scanner = new Scanner(System.in);
 
@@ -19,10 +32,21 @@ public class Main {
         }
     }
 
+    /**
+     * System.out.printをいちいち書くのが面倒なので、短く書けるように作りました。
+     * @param string
+     */
     private static void print(String string){
         System.out.print(string);
     }
 
+    /**
+     * ハッシュ値を返すメソッド。
+     * @param UsingHashInstance 使用するハッシュのMessageDigest型変数
+     * @param content ハッシュにしたいString型の原文
+     * @return ハッシュ化されたString型変数
+     * @throws NoSuchAlgorithmException 指定されたハッシュ関数名が存在しません
+     */
     private static String Tohash(MessageDigest UsingHashInstance,String content) throws NoSuchAlgorithmException {
         String StringFormat = "%040x";
         if(UsingHashInstance == MessageDigest.getInstance(HashTypes[0])) StringFormat = "%020x";
